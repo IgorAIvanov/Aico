60aff0415036c18632c92c804b1c2e2ba49d997e
igova <igovano@gmail.com>
2025-05-03 11:35:55 +0300

    redirect 3

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ const app = new Hono()
 app.get('/', (c) => {
   //console.log(c.req.url);
   if (c.req.url.includes('www')){
-     {c.redirect('https://aicoai.com.ua')}
+     {c.redirect('https://aicoai.com.ua/')}
      return;
   }
   return c.html(Deno.readTextFileSync('./static/landing.html'))
