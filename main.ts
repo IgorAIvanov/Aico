52ced76ff352ec76790d76aed29004ae31d1e959
igova <igovano@gmail.com>
2025-05-03 11:28:21 +0300

    redirect2

--- main.ts
+++ main.ts
@@ -5,8 +5,11 @@ import { serveStatic } from 'hono/deno'
 const app = new Hono()
 
 app.get('/', (c) => {
-  if (c.req.url.includes('www'))
+  //console.log(c.req.url);
+  if (c.req.url.includes('www')){
      {c.redirect('https://aicoai.com.ua')}
+     return;
+  }
   return c.html(Deno.readTextFileSync('./static/landing.html'))
 })
 
