77458c7b677aff78d6421a52fc6f9f85eb15d4a2
igova <igovano@gmail.com>
2025-05-03 11:06:50 +0300

    Landing

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { serveStatic } from 'hono/deno'
+
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Leathers!')
+  return c.html(Deno.readTextFileSync('./static/landing.html'))
 })
 
 Deno.serve(app.fetch)
